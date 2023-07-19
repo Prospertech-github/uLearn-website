868b1ed7d2d81e3d4790492972e57f0cd0b6d7ad
Prosper Ikechukwu <mrhendrix62@gmail.com>
2023-07-19 14:10:14 +0100

    FIX: Clear comments on Login.js file

--- login.js
+++ login.js
@@ -33,55 +33,7 @@ loginForm.addEventListener('submit', e => {
     }
   }
   
-  // if (emailDetails.toLowerCase().includes('@') && passwordDetails.length >= 8) {
-  //   const userDetails = {
-  //     userEmail: emailDetails,
-  //     userPassword: passwordDetails,
-  //   };
-  //   user.push(userDetails);
-    
-  //   localStorage.setItem('user', JSON.stringify(user));
-
-  //   alert('Please wait while we log you in');
-
-  //   setTimeout(() => {
-  //     window.location.href="index.html"
-  //   }, 5000)
-  // } else {
-  //   alert('Please input a valid email addrress or password. \n Passwords must be atleast 8 characters or more')
-  // }
-
-  
-
-  
   console.log(emailDetails)
 	console.log(passwordDetails)
   console.log('Login Form Submitted');
-
-
 });
-
-
-// check if newSignUp exists
-// if (localStorage.getItem('newSignUps' !== null)) {
-  //fetch Details
-//   const userDetails = JSON.parse(localStorage.getItem('newSignUps'))
-//   const emailDetails = email.value;
-//   const passwordDetails = password.value;
-
-//   if (emailDetails = userDetails[0].email) {
-//     const loggedInUser = {
-//       userEmail: emailDetails,
-//       password: passwordDetails,
-//       firstName: userDetails[0].firstName
-//     } 
-//     user.push(loggedInUser)
-//     localStorage.setItem('loggedInUserDetails', user);
-//   }
-// } else {
-//   alert('OOPS!! It seems like you don\'t have a ULear aaccount at this time \n Please signup now, we want to serve you')
-// }
-// create user storage
-// fill in details
-// check if email input exists
-// get 
